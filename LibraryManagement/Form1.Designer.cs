2742dec41029643256ec65c34fc4181b67af1419
BuiHuyTam <ht2812002@gmail.com>
2024-04-09 21:18:00 +0700

    feat: first commit

---
+++ LibraryManagement/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace LibraryManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            btnLogin = new Button();
+            button2 = new Button();
+            btnClose = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(122, 44);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(91, 84);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(106, 182);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(189, 27);
+            txtUsername.TabIndex = 1;
+            txtUsername.MouseClick += txtUsername_MouseClick;
+            txtUsername.MouseEnter += txtUsername_MouseEnter;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(106, 228);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(189, 27);
+            txtPassword.TabIndex = 2;
+            txtPassword.MouseClick += txtPassword_MouseClick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(25, 182);
+            label1.Name = "label1";
+            label1.Size = new Size(75, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Username";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = SystemColors.ButtonHighlight;
+            label2.Location = new Point(25, 228);
+            label2.Name = "label2";
+            label2.Size = new Size(70, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Password";
+            label2.Click += label2_Click;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.White;
+            btnLogin.FlatStyle = FlatStyle.Popup;
+            btnLogin.Location = new Point(38, 309);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(257, 38);
+            btnLogin.TabIndex = 5;
+            btnLogin.Text = "Log in";
+            btnLogin.UseVisualStyleBackColor = false;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.White;
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.Location = new Point(38, 365);
+            button2.Name = "button2";
+            button2.Size = new Size(257, 38);
+            button2.TabIndex = 6;
+            button2.Text = "Sign up";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // btnClose
+            // 
+            btnClose.BackColor = Color.White;
+            btnClose.FlatStyle = FlatStyle.Popup;
+            btnClose.Location = new Point(287, 12);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(32, 29);
+            btnClose.TabIndex = 7;
+            btnClose.Text = "X";
+            btnClose.UseVisualStyleBackColor = false;
+            btnClose.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(35, 36, 55);
+            ClientSize = new Size(331, 453);
+            Controls.Add(btnClose);
+            Controls.Add(button2);
+            Controls.Add(btnLogin);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private Label label1;
+        private Label label2;
+        private Button btnLogin;
+        private Button button2;
+        private Button btnClose;
+    }
+}
---
+++ LibraryManagement/Form1.cs
@@ -0,0 +1,51 @@
+namespace LibraryManagement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        //btnClose
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtUsername_MouseEnter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUsername_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (txtUsername.Text == "Username")
+            {
+                txtUsername.Clear();
+            }
+        }
+
+        private void txtPassword_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (txtUsername.Text == "Password")
+            {
+                txtPassword.Clear();
+                txtPassword.PasswordChar = '*';
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
