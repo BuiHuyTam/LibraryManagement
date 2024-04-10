839eac17cf701d8bfe7e7a70570de7686c8208e4
BuiHuyTam <ht2812002@gmail.com>
2024-04-10 22:53:23 +0700

    feat: add login function

--- LibraryManagement/Form1.cs
+++ LibraryManagement/Form1.cs
@@ -1,3 +1,6 @@
+using System.Data;
+using System.Data.SqlClient;
+
 namespace LibraryManagement
 {
     public partial class Form1 : Form
@@ -45,7 +48,25 @@ namespace LibraryManagement
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "data source = BHTAM\\SQLEXPRESS; database=LibraryManagement; integrated security=True";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "select * from LoginTable where username ='"+txtUsername.Text+"' and pass = '"+txtPassword.Text+"' ";
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            if (ds.Tables[0].Rows.Count != 0)
+            {
+                this.Hide();
+                Dashboard dsa = new Dashboard();
+                dsa.Show();
+            }
+            else
+            {
+                MessageBox.Show("Wrong Username OR Password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            int a = 5;
         }
     }
 }
